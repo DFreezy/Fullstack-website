e321b51b56334ac068b41a57f557fc3184747caa
CodeSpace Academy <duwaynefrieslaar23@gmail.com>
2024-11-02 23:10:09 +0200

    fix: router
    Fixed routing

--- my-blog-site/src/Pages/Start.jsx
+++ my-blog-site/src/Pages/Start.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function Start(){
-    <>
-    <div>
-        <h1>Hi there!</h1>
-        <img src="/public/WhatsApp Image 2024-10-07 at 13.13.10_bd5a4536.jpg"/>
-        <h1>Picture this</h1>
-    </div>
-    </>
+    return(
+        <>
+        <div className="flex flex-row justify-between mr-64">
+            <img src="/public/WhatsApp Image 2024-10-07 at 13.13.10_bd5a4536.jpg" width="400" height="200"/>
+            <div className="flex flex-col">
+            <h1 className="font-bold text-xl">Have you ever asked yourself?</h1>
+            <p>How am I going to afford a website..?</p>
+            <p>Will it be good enough..?</p>
+            <p>Is it worth the investment..?</p>
+            </div>
+        </div>
+        </>
+    )
 }
--- my-blog-site/tailwind.config.js
+++ my-blog-site/tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     "./src/App.jsx",
     "./src/components/navbar.jsx",
     "./src/Pages/Homepage.jsx",
+    "./src/Pages/Start.jsx",
   ],
   theme: {
     extend: {},
